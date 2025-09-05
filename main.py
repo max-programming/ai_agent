baacb444438c744c18b29f67450059cf6406bcaa
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 02:28:33 +0000

    store messages in a list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -12,13 +13,17 @@ def main():
         print("No prompt provided")
         sys.exit(1)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
 
     if response is None or response.usage_metadata is None:
