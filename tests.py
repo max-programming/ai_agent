66ba9bbe31164db4ecad2dba9934c0fed8d53e63
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 02:56:47 +0000

    added a get_files_info function

---
+++ tests.py
@@ -0,0 +1,16 @@
+from functions.get_files_info import get_files_info
+
+
+def main():
+    working_dir = "calculator"
+    root_contents = get_files_info(working_dir)
+    print(root_contents)
+    pkg_contents = get_files_info(working_dir, "pkg")
+    print(pkg_contents)
+    bin_contents = get_files_info(working_dir, "bin")
+    print(bin_contents)
+    back_directory = get_files_info(working_dir, "../")
+    print(back_directory)
+
+
+main()
