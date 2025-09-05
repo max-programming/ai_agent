f15490e0091d8f76b271bc5abb371fc704f7c628
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 02:22:31 +0000

    call the main function

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ def main():
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="""
-      Why is Boot.dev such a great place to learn backend development?
-      Use one paragraph maximum.
-    """,
+        contents="""Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.""",
     )
 
     if response is None or response.usage_metadata is None:
@@ -25,3 +22,6 @@ def main():
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+main()
