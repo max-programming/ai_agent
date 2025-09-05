53ca5f6b9c7c8b43ff8ee1478a01c1a7a3256618
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 15:25:21 +0000

    Add configuration for max character limit, implement get_file_content function, and update tests to reflect new functionality

--- tests.py
+++ tests.py
@@ -1,16 +1,12 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def main():
     working_dir = "calculator"
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    bin_contents = get_files_info(working_dir, "bin")
-    print(bin_contents)
-    back_directory = get_files_info(working_dir, "../")
-    print(back_directory)
+    print(get_file_content(working_dir, "main.py"))
+    print(get_file_content(working_dir, "pkg/calculator.py"))
+    print(get_file_content(working_dir, "/bin/cat"))
+    print(get_file_content(working_dir, "pkg/does_not_exist.py"))
 
 
 main()
