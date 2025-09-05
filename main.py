a2e78d9dd5fd30e5da388ab1973cd1c60493607b
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 02:25:38 +0000

    take prompt as command argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,13 +7,18 @@ load_dotenv()
 
 
 def main():
+    prompt = sys.argv[1]
+    if prompt is None:
+        print("No prompt provided")
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="""Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.""",
+        contents=prompt,
     )
 
     if response is None or response.usage_metadata is None:
