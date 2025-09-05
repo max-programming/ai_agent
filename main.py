5e44d5a37810ddeb3a5cce81e68ac0df3d003026
Usman S. <ui.sabuwala@gmail.com>
2025-09-05 02:21:28 +0000

    basic gemini usage

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+
 def main():
-    print("Hello from ai-agent!")
+    api_key = os.environ.get("GEMINI_API_KEY")
 
+    client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="""
+      Why is Boot.dev such a great place to learn backend development?
+      Use one paragraph maximum.
+    """,
+    )
+
+    if response is None or response.usage_metadata is None:
+        print("No response from the model")
+        return
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
