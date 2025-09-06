50a3951b449e685f67e1cf378f573b83a44bd4c0
Usman S. <ui.sabuwala@gmail.com>
2025-09-06 06:03:45 +0000

    write_file function

--- tests.py
+++ tests.py
@@ -1,12 +1,11 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def main():
     working_dir = "calculator"
-    print(get_file_content(working_dir, "main.py"))
-    print(get_file_content(working_dir, "pkg/calculator.py"))
-    print(get_file_content(working_dir, "/bin/cat"))
-    print(get_file_content(working_dir, "pkg/does_not_exist.py"))
+    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
 
 
 main()
