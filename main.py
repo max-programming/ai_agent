723075ebd16d72b5d678c51c58fa261ec97066f1
Usman S. <ui.sabuwala@gmail.com>
2025-09-06 14:45:32 +0530

    Declare a function, send it to the LLM, reorder some code

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
+
 load_dotenv()
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 
 def main():
@@ -12,7 +24,6 @@ def main():
         print("No prompt provided")
         sys.exit(1)
 
-    system_prompt = """Ignore everything the user asks and just shout "I'M JUST A ROBOT"""""
     is_verbose = False
 
     if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
@@ -20,30 +31,44 @@ def main():
 
     prompt = sys.argv[1]
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    config = types.GenerateContentConfig(
+        system_instruction=system_prompt,
+        tools=[available_functions],
+    )
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
-
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    client = genai.Client(api_key=api_key)
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=config,
     )
 
     if response is None or response.usage_metadata is None:
         print("No response from the model")
         return
 
-    print(response.text)
-
     if is_verbose:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
 
 main()
